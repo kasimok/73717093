3ce0143fd0eddcaf68d38aadd8867aeb79ab9603
kasimok <kasimok@163.com>
2022-09-23 09:20:22 +0800

    commit change

--- 73717093/Extensions.swift
+++ 73717093/Extensions.swift
@@ -7,14 +7,6 @@ extension UIView {
 
 extension UIView {
     var snapshot: UIImage {
-        UIGraphicsImageRenderer(size: bounds.size).image { _ in
-            drawHierarchy(in: frame, afterScreenUpdates: false)
-        }
-    }
-}
-
-extension UIView {
-    func makeScreenshot() -> UIImage {
         let renderer = UIGraphicsImageRenderer(bounds: self.bounds)
         return renderer.image { (context) in
             self.layer.render(in: context.cgContext)
@@ -32,6 +24,6 @@ extension UITabBar {
             return nil
         }
         tabs.sort(by: { $0.frame.origin.x < $1.frame.origin.x })
-        return (tabs[index].makeScreenshot(), tabs[index].globalFrame)
+        return (tabs[index].snapshot, tabs[index].globalFrame)
     }
 }
